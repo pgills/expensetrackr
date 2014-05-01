ed64981c9525ae431f42129fdb5f2a1bfaa1ed2e
Edgar Banguero <edgar@live.com>
2014-04-30 17:12:48 -0700

    adding android project

---
+++ platforms/android/assets/www/js/core/services/locationService.js
@@ -0,0 +1,41 @@
+﻿(function (WinJS) {
+    WinJS.Namespace.define("App.Services",
+        {
+            Location: WinJS.Class.define(function ctor() {
+                // Check to see if have the Cordova library
+                // If not just use HTML geo-loc
+                this._location = {
+                    getCurrentPosition: function (success, error) {
+                        var position = {
+                            coords: {
+                                longitude: 0,
+                                latitude: 0
+                            }
+                        };
+                        success(position);
+                    }
+                }
+
+                if (navigator.geolocation) {
+                    this._location = navigator.geolocation;
+                }
+
+            },
+            {
+                getCurrentPosition: function () {
+
+                    var that = this;
+                    var p = new WinJS.Promise(function (c, e) {
+                        that._location.getCurrentPosition(function (success) {
+                            c(success);
+                        }, function (error) {
+                            e(error);
+                        });
+                    });
+
+                    return p;
+                }
+            })
+        });
+
+}(WinJS));
---
+++ platforms/android/assets/www/js/core/unittest/service/expenses.js
@@ -0,0 +1,59 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    UserControl.define("/www/js/core/unittest/service/expenses.html", {
+
+        init: function (element, options) {
+            this._service = new App.Services.ExpenseService();
+            this.list = WinJS.UI.eventHandler(this._list.bind(this));
+            this.add = WinJS.UI.eventHandler(this._add.bind(this));
+            
+        },
+
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        },
+
+        _list: function (ev) {
+            var that = this;
+            
+        
+            console.log("Getting expenses");
+            this._service.list().then(function (list) {
+                that.output = "Success";
+                console.log(that.output);
+            })
+        },
+
+        _add: function (ev) {
+            var exp = {
+                title: "Expense 1",
+                text: "This is the description for the expense",
+                long: "0",
+                lat: "1",
+            };
+
+            console.log("Inserting item")
+            this._service.push(exp).then(function (success) {
+                console.log("Success: " + success);
+            }, function (error) {
+                console.log("Error: " + error);
+            });
+        }
+    });
+})();
