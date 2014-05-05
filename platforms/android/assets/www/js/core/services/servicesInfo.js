4ea7992abf78ee79655d498bf61c917ac77097f2
Edgar Banguero <edgar@live.com>
2014-05-05 16:14:46 -0700

    fixes

---
+++ platforms/android/assets/www/js/core/services/servicesInfo.js
@@ -0,0 +1,12 @@
+WinJS.Namespace.define("App.Services.Info", {
+                       
+    // Bing Maps API Key
+    BingMapsApiKey: "Ah3HFyLsjxFXG_nUBvvA2F1DEupYsBLn_EPMRAjwt1XHhMkR-XJPZGjbyXlDRUNO",
+    
+    // Azure Mobile Services API Key
+    AzureMobileServicesApiKey: "AtFyFCVeANFXiKZEzyENpGkjHhOvfp99",
+    
+    // Azure Mobile Services Url
+    AzureMobileServicesUrl: "https://snc.azure-mobile.net/"
+
+});
