3e2a8bb63695c1fa4144c9fbe9290126e77cf578
Edgar Banguero <edgar@live.com>
2014-05-05 14:11:23 -0700

    more cleanup

--- www/js/core/views/expenses/expenses.js
+++ www/js/core/views/expenses/expenses.js
@@ -13,17 +13,14 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
         },
 
         unload: function () {
-            // TODO: Respond to navigations away from this page.
         },
 
         updateLayout: function (element) {
             /// <param name="element" domElement="true" />
             this.listView.winControl.forceLayout();
-            // TODO: Respond to changes in layout.
         },
 
         _itemInvoked: function (ev) {
