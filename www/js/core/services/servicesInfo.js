d7f63fdab04bca54f3066d195650daacc6e84266
Edgar Banguero <edgar@live.com>
2014-05-05 15:56:59 -0700

    commenting alert for iOS

--- www/js/core/services/servicesInfo.js
+++ www/js/core/services/servicesInfo.js
@@ -3,8 +3,10 @@ WinJS.Namespace.define("App.Services.Info", {
     // Bing Maps API Key
     BingMapsApiKey: "Ah3HFyLsjxFXG_nUBvvA2F1DEupYsBLn_EPMRAjwt1XHhMkR-XJPZGjbyXlDRUNO",
     
-    AzureMobileServicesApiKey: "AtFyFCVeANFXiKZEzyENpGkjHhOvfp99", //edgar: "MxCEUWTPhyviGiPezKqnJeZGXWOKVq97", //pauls: "AtFyFCVeANFXiKZEzyENpGkjHhOvfp99"
+    // Azure Mobile Services API Key
+    AzureMobileServicesApiKey: "AtFyFCVeANFXiKZEzyENpGkjHhOvfp99",
     
-    AzureMobileServicesUrl: "https://snc.azure-mobile.net/" //edgar"https://edgar.azure-mobile.net/", //pauls: "https://snc.azure-mobile.net/"
+    // Azure Mobile Services Url
+    AzureMobileServicesUrl: "https://snc.azure-mobile.net/"
 
 });
